f7f4a8ae006402d383c554c4f8dd0d21f0e9a081
WesyPlayz <rharding1022@gmail.com>
2026-01-15 18:00:02 -0800

    UPDATE Project
    V0.14 Project Updated

---
+++ src/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿#region GENERAL HEADER
+
+using System.Windows;
+
+#endregion
+#region LANCHAT HEADER
+
+using LanChat.NetworkSystem;
+
+#endregion
+
+namespace LanChat.Runtime;
+
+public partial class App : Application 
+{
+    protected override void OnStartup ( StartupEventArgs _ )
+    {
+        MainWindow = new MainWindow(); 
+        MainWindow.Show();
+
+        Bridge.Init_Authentication();
+    }
+}
